9d9aa1e424e2d6138376514560ec2471b807ab87
Meimei07 <mmey73069@gmail.com>
2025-08-23 10:55:11 +0700

    git init and setup project

---
+++ BlogPost/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogPost.Models
+{
+    public class AppDbContext(IConfiguration configuration) : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("conn"));
+            optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+        }
+    }
+}
