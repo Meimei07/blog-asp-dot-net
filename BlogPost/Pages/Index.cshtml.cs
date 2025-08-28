581d491e1e52d71583a0741dd60fd403e7b859b1
Meimei07 <mmey73069@gmail.com>
2025-08-28 21:23:43 +0700

    update homepage and layout

--- BlogPost/Pages/Index.cshtml.cs
+++ BlogPost/Pages/Index.cshtml.cs
@@ -12,6 +12,12 @@ namespace BlogPost.Pages
 
         public IEnumerable<PostEntity> Posts {get;set;}
 
+        [BindProperty]
+        public IEnumerable<TagEntity> Tags {get;set;}
+
+        [BindProperty]
+        public string SearchText {get;set;} = "";
+
         public IndexModel(ILogger<IndexModel> logger, AppDbContext db)
         {
             _logger = logger;
@@ -23,7 +29,30 @@ namespace BlogPost.Pages
             Posts = await _db.Posts
                 .Include(p => p.Category)
                 .Include(p => p.Tags)
+                .Include(p => p.User)
                 .ToListAsync();
+
+            Tags = await _db.Tags.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostSearch()
+        {
+            if (string.IsNullOrEmpty(SearchText))
+            {
+                return RedirectToPage();
+            }
+
+            Posts = await _db.Posts
+                .Include(p => p.Category)
+                .Include(p => p.Tags)
+                .Include(p => p.User)
+                .Where(p => p.Title.Contains(SearchText) || // search by post title
+                        p.Category.Name.Contains(SearchText) || // by category name
+                        p.Tags.Any(t => t.Name.Contains(SearchText))) // by tag name
+                .ToListAsync();
+
+            Tags = await _db.Tags.ToListAsync();
+            return Page();
         }
     }
 }
